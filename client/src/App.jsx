5ea366d74857bcb1a4a1c4bd3f7a32e0ae5a1858
Ankit-976 <ankitbasa113@gmail.com>
2026-05-06 19:58:16 +0530

    Installed all dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-black text-white h-screen flex items-center justify-center text-4xl font-bold'>
       App
     </div>
   )
