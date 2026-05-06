71547b3d715299f3f0eac87740cc2f2825b49fda
Ankit-976 <ankitbasa113@gmail.com>
2026-05-06 14:42:24 +0530

    Created multer config

---
+++ server/src/config/multer.js
@@ -0,0 +1,30 @@
+const multer = require("multer");
+
+const upload = multer({
+
+  storage: multer.memoryStorage(),
+
+  limits: {
+    fileSize: 5 * 1024 * 1024
+  },
+
+  fileFilter: (req, file, cb) => {
+
+    const allowed = [
+      "image/jpeg",
+      "image/png",
+      "image/webp"
+    ];
+
+    if (allowed.includes(file.mimetype)) {
+
+      cb(null, true);
+
+    } else {
+
+      cb(new Error("Invalid file type"));
+    }
+  }
+});
+
+module.exports = upload;
