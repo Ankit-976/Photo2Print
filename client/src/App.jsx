ca9506bdf47c4e01c2815e3cd89fa2aba000c040
Ankit-976 <ankitbasa113@gmail.com>
2026-05-06 20:45:00 +0530

    First impression of website

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-black text-white h-screen flex items-center justify-center text-4xl font-bold'>
+    <div className='bg-black text-white h-screen text-4xl font-bold p-5'>
       App
     </div>
   )
