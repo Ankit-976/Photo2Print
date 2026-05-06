d4f7d515c84ef86cdfa3359ef5ea1c279e2d7a1f
Ankit-976 <ankitbasa113@gmail.com>
2026-05-06 14:57:58 +0530

    Added route

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const photoRoutes = require('./routes/photoRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/photos', photoRoutes);
+
+
 
 module.exports = app;
