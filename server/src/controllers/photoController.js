43a406e4777a4c96ac9a41245cb6a70c75535ea1
Ankit-976 <ankitbasa113@gmail.com>
2026-05-06 15:11:56 +0530

    fixed error

--- server/src/controllers/photoController.js
+++ server/src/controllers/photoController.js
@@ -1,31 +1,3 @@
-// const sharp = require("sharp");
-
-// const uploadPhoto = async (req, res) => {
-
-//   try {
-
-//     const metadata = await sharp(req.file.buffer)
-//       .metadata();
-
-//     res.json({
-//       message: "Upload successful",
-//       metadata
-//     });
-
-//   } catch (error) {
-
-//     console.log(error);
-
-//     res.status(500).json({
-//       message: "Something went wrong"
-//     });
-//   }
-// };
-
-// module.exports = {
-//   uploadPhoto
-// };
-
 const generateSheet = require("../utils/generateSheet");
 
 const uploadPhoto = async (req, res) => {
